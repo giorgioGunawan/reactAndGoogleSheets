a985d78fd00fe6a0551eb50dc5c3635a5ce60f7a
giorgioGunawan <grggnwn@gmail.com>
2021-03-01 17:00:48 +1100

    revert

--- react-google-sheets/src/App.js
+++ react-google-sheets/src/App.js
@@ -76,48 +76,10 @@ class App extends React.Component {
     
     
     var someData = []
-
-    const useStyles = makeStyles((theme) => ({
-      root: {
-        flexGrow: 1,
-      },
-      paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      },
-    }));
-    
-    const classes = useStyles();
-    
+      
     return (
       
       <div>
-        <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=12</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>xs=3</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>xs=3</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>xs=3</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>xs=3</Paper>
-            </Grid>
-          </Grid>
-        </div>
         <div>
           {
             data.map(obj => {
